5fe1d602b3439e59051653bfe35b62b7adea12bc
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-19 14:37:56 +0300

    save

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+console.log(galleryItems);
 
+
+
+const gallery = document.querySelector('.gallery');
+
+const makeGallery = createItemsPicture(galleryItems);
+
+function createItemsPicture(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    }).join("");
+}
+
+gallery.insertAdjacentHTML("beforeend", makeGallery);
+
+gallery.addEventListener('click', onOpenModal);
+
+function onOpenModal(evt) {
+    evt.preventDefault();
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    captionsData: "alt",
+    captionDelay: 250
+ });
+
+gallery.addEventListener("keydown", closeModal);
+
+function closeModal(evt) {
+    if (evt.code === 'Escape') {
+        instance.close();
+    }
+};
 console.log(galleryItems);
