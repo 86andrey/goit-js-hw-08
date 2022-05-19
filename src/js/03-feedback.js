bab838679cc4068d157b113cc4d35204ac2c5b20
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-19 23:28:06 +0300

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,11 +1,12 @@
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные
-// данные, заполняй ими поля формы.В противном случае поля должны быть пустыми.
+import throttle from 'lodash.throttle';
 
 const email = document.querySelector('.feedback-form input');
 const message = document.querySelector('.feedback-form textarea');
 const form = document.querySelector('.feedback-form');
 
-form.addEventListener('input', onFieldsInput);
+populateTextarea();
+
+form.addEventListener('input', throttle(onFieldsInput, 500));
 form.addEventListener('submit', onFormSubmit);
 
 function onFieldsInput() {
@@ -21,4 +22,13 @@ function onFormSubmit(event) {
   event.currentTarget.reset();
   console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
   localStorage.removeItem('feedback-form-state');
+}
+
+function populateTextarea() {
+  const savedMessage = localStorage.getItem('feedback-form-state');
+  if (savedMessage) {
+    const savedDataForm = JSON.parse(savedMessage);
+    email.value = savedDataForm.email;
+    message.value = savedDataForm.message;
+  }
 }
