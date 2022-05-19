579fb7ab21995e5cba5e7c325dc21c40d41306b2
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-19 18:55:32 +0300

    save

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -28,13 +28,3 @@ player
         break;
     }
   });
-
-const a = player
-  .getCurrentTime()
-  .then(function (seconds) {
-    // seconds = the current playback position
-  })
-  .catch(function (error) {
-    // an error occurred
-  });
-console.log(a.seconds);
