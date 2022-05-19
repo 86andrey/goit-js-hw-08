99f794f2c01f7f0aed903608ed6a6d1ace69944a
Andrey Ivanov <prioritet2503@gmail.com>
2022-05-19 22:04:11 +0300

    save

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,24 @@
-// Отслеживай на форме событие input, и каждый раз записывай в
-// локальное хранилище объект с полями email и message, в которых
-//  сохраняй текущие значения полей формы.
-//  Пусть ключом для хранилища будет строка "feedback-form-state".
+// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные
+// данные, заполняй ими поля формы.В противном случае поля должны быть пустыми.
 
-const refs = {
-  email: document.querySelector('.feedback-form email'),
-  message: document.querySelector('.feedback-form message'),
-  form: document.querySelector('.feedback-form'),
-};
+const email = document.querySelector('.feedback-form input');
+const message = document.querySelector('.feedback-form textarea');
+const form = document.querySelector('.feedback-form');
 
-// refs.email.addEventListener('input', onEmailInput);
+form.addEventListener('input', onFieldsInput);
+form.addEventListener('submit', onFormSubmit);
 
-refs.message.addEventListener('input', onMessageInput);
-
-// refs.form.addEventListener('submit', onFormSubmit);
-
-function onEmailInput() {}
-
-function onMessageInput(event) {
-  const value = event.currentTarget.value;
-  console.log(value);
-  //   localStorage.setItem('feedback-form-state', string);
+function onFieldsInput() {
+  const createObjForm = {
+    email: email.value,
+    message: message.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(createObjForm));
 }
 
-function onFormSubmit() {}
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+}
